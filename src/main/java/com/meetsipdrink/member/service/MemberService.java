c9bf607f0165620bb6d001f68e3c2ddda76ec892
JinHyeongSeo <jh4clover@naver.com>
2024-09-05 23:22:52 +0900

    #22 SecurityConfiguration permit all 나중에 변경 필요, MemberService password encording 추가

--- src/main/java/com/meetsipdrink/member/service/MemberService.java
+++ src/main/java/com/meetsipdrink/member/service/MemberService.java
@@ -1,13 +1,16 @@
 package com.meetsipdrink.member.service;
 
+import com.meetsipdrink.auth.utils.CustomAuthorityUtils;
 import com.meetsipdrink.exception.BusinessLogicException;
 import com.meetsipdrink.exception.ExceptionCode;
 import com.meetsipdrink.member.entity.Member;
 import com.meetsipdrink.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,10 +18,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final CustomAuthorityUtils authorityUtils;
 
     public Member ceateMember(Member member) {
         verifyExistMember(member.getEmail());
         verifyNickName(member.getNickname());
+
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encryptedPassword);
+
+        List<String> roles = authorityUtils.createRoles(member.getEmail());
+        member.setRoles(roles);
+
         Member verifiedMember = memberRepository.save(member);
         return verifiedMember;
 
@@ -45,7 +57,7 @@ public class MemberService {
     }
 
 
-    
+
 
     private void verifyExistMember(String email) {
         Optional<Member> member = memberRepository.findByEmail(email);
