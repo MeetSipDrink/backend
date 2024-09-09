2f54eca4c4e63edf49588d724b28b34a9daa6717
coding-Rhino <qkdrb1234@naver.com>
2024-09-09 12:34:33 +0900

    #51 NoticeRepository 구현

---
+++ src/main/java/com/meetsipdrink/notice/repository/NoticeImageRepository.java
@@ -0,0 +1,7 @@
+package com.meetsipdrink.notice.repository;
+
+import com.meetsipdrink.notice.entity.NoticeImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeImageRepository extends JpaRepository<NoticeImage, Long> {
+}
